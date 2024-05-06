cbc1672dfccd3eb0c1aae55b7bb02a9a3d0cd73c
HeoJamong <84281455+HeoJamong@users.noreply.github.com>
2024-05-07 08:22:48 +0900

    [Silver III] Title: N과 M (3), Time: 2860 ms, Memory: 142084 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,14 @@
+N,M = map(int, input().split())
+
+ans = []
+
+def dfs(n, lst):
+    if n == M:
+        ans.append(lst)
+        return
+    for i in range(1, N+1):
+        dfs(n+1, lst+[i])
+
+dfs(0,[])
+for lst in ans:
+    print(*lst)
