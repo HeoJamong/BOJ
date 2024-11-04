906ac142aa942a2ac06bd76457a0638e23c34717
HeoJamong <84281455+HeoJamong@users.noreply.github.com>
2024-11-05 00:18:41 +0900

    [Silver III] Title: 소수 구하기, Time: 136 ms, Memory: 41332 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,14 +1,26 @@
 import sys
+input = sys.stdin.read
 
-input = sys.stdin.readline
+def sieve_of_eratosthenes(M, N):
+    # 0과 1은 소수가 아니므로 False로 설정
+    sieve = [True] * (N + 1)
+    sieve[0] = sieve[1] = False
+    
+    # 에라토스테네스의 체 알고리즘 적용
+    for i in range(2, int(N ** 0.5) + 1):
+        if sieve[i]:  # i가 소수일 때
+            for j in range(i * i, N + 1, i):
+                sieve[j] = False  # i의 배수를 모두 False로 설정
 
-m, n = map(int, input().split())
+    # M부터 N까지의 소수 출력
+    primes = []
+    for k in range(M, N + 1):
+        if sieve[k]:
+            primes.append(k)
+    
+    return primes
 
-for i in range(m, n+1):
-    if i == 1:
-        continue
-    for j in range(2, int(i**0.5) + 1):
-        if i % j == 0:
-            break
-    else:
-        print(i)
+# 입력받기
+M, N = map(int, input().split())
+primes = sieve_of_eratosthenes(M, N)
+print("\n".join(map(str, primes)))
