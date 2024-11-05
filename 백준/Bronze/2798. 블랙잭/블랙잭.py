9002bbd949df8485d250e7263b3f7db7f432b96d
HeoJamong <84281455+HeoJamong@users.noreply.github.com>
2024-11-05 11:39:14 +0900

    [Bronze II] Title: 블랙잭, Time: 72 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,17 +1,14 @@
 import sys
+from itertools import combinations
 
 input = sys.stdin.readline
-n,m = map(int, input().split())
+n, m = map(int, input().split())
 arr = list(map(int, input().split()))
+max_sum = 0
 
-sum = 0
-for i in range(n - 2):
-    for j in range(i + 1, n - 1):
-        for k in range(j + 1, n):
-            if sum == m:
-                print(sum)
-                sys.exit()
-            tmp = arr[i] + arr[j] + arr[k]
-            if tmp <= m and sum < tmp:
-                sum = tmp
-print(sum)
+for comb in combinations(arr, 3):
+    tmp = sum(comb)
+    if tmp <= m and max_sum < tmp:
+        max_sum = tmp
+
+print(max_sum)
